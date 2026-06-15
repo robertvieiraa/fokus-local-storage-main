542968de422d6ffde348bdd3376976e6ea6b3bbc
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-06-15 20:44:23 -0300

    Adicionei funcionalidade de cancelar criação de tarefa

--- script-crud.js
+++ script-crud.js
@@ -2,6 +2,7 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
 
@@ -68,4 +69,9 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
 tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
+})
+
+btnCancelar.addEventListener('click', () => {
+    textarea.value = ''
+    formAdicionarTarefa.classList.add('hidden')
 })
