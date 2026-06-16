0a0155aaa023e18f3d4624e03731785aab0fdcb1
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-06-16 19:51:04 -0300

    Adicionei função de desselecionar uma tarefa

--- script-crud.js
+++ script-crud.js
@@ -6,6 +6,7 @@ const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
@@ -48,6 +49,15 @@ function criarElementoTarefa(tarefa) {
     li.append(botao)
 
     li.onclick = () => {
+        document.querySelectorAll('.app__section-task-list-item-active').forEach(elemento => {
+            elemento.classList.remove('app__section-task-list-item-active')
+        })
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            return
+        }
+        tarefaSelecionada = tarefa
         paragrafoDescricaoTarefa.textContent = tarefa.descricao
         li.classList.add('app__section-task-list-item-active')
     }
